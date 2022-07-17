7bca9183249d99091d471bf5745f16cb0f159b22
Monineath Sar <monineathsar@gmail.com>
2022-07-16 17:21:05 -0700

    saving and rendering notes works

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 // dependencies
+const { Console } = require('console');
+const { json } = require('express');
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 const notesData = require('./db/db.json');
+const dbFilePath = path.join(__dirname, "/db/db.json");
 
 // express server & PORT
 const app = express();
@@ -19,16 +23,48 @@ app.get('/notes', (req, res) => {
 });
 
 
+// Get request to return conent from JSON file
+app.get('/api/notes', (req, res) => {
+    res.sendFile(dbFilePath);
+});
 
 // POST request for new notes
 app.post('/api/notes', (req, res) => {
     res.json(`${req.method} request recieved to add a note`);
+    let newNote = {
+        title: req.body.title,
+        text: req.body.text
+    }
+    
+    if (newNote.title && newNote.text) {
+        // grab existing reviews
+        fs.readFile(dbFilePath, 'utf-8', (err, data) => {
+            if (err) {
+                res.status(404).json('File not found');
+            } else {
+                // convert string into JSON object
+                const parsedNote = JSON.parse(data);
+                // adds the new note
+                parsedNote.push(newNote);
+
+                // writes new notes into the file
+                fs.writeFile(
+                    dbFilePath,
+                    JSON.stringify(parsedNote),
+                    (writeErr) => 
+                        writeErr
+                            ? console.error(writeErr)
+                            : console.info('Successfully updated notes!')
+                );
+            }
+        });
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in posting note');
+    }
 }); 
 
-// Get request to return conent from JSON file
-app.get('/api/notes', (req, res) => {
-    res.json(notesData)
-});
+
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
